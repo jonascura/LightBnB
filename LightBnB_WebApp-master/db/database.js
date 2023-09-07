ea4b774f02a38c0566e465e2fce3cb79b4dc2fe6
Jonas Cura <jonasm.cura@gmail.com.com>
2023-09-07 00:53:46 -0600

    Refactor getAllProperties to enable filtering and return necessary data

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -98,16 +98,55 @@ const getAllReservations = function (guest_id, limit = 10) {
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-const getAllProperties = (options, limit = '10') => {
-  return pool
-    .query(`SELECT * FROM properties LIMIT $1;`, [limit])
-    .then((result) => {
-      console.log(result.rows);
-      return result.rows;
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
+const getAllProperties = (options, limit = 10) => {
+    // 1
+    const queryParams = [];
+    // 2
+    let queryString = `
+    SELECT properties.*, avg(property_reviews.rating) as average_rating
+    FROM properties
+    JOIN property_reviews ON properties.id = property_id
+    `;
+  
+    // 3
+    if (options.city) {
+      queryParams.push(`%${options.city}%`);
+      queryString += `WHERE city LIKE $${queryParams.length} `;
+    }
+
+    if (options.owner_id) {
+      queryParams.push(`${options.owner_id}`);
+      queryString += `AND owner_id = $${queryParams.length} `;
+    }
+
+    if (options.minimum_price_per_night && options.maximum_price_per_night) {
+      queryParams.push(`${Number(options.maximum_price_per_night)}` * 100);
+      queryString += `AND cost_per_night >= $${queryParams.length} `;
+      queryParams.push(`${Number(options.minimum_price_per_night)}` * 100);
+      queryString += `AND cost_per_night <= $${queryParams.length} `;
+    }
+  
+    // 4
+    queryString += `
+    GROUP BY properties.id
+    `;
+
+    if (options.minimum_rating) {
+      queryParams.push(`${options.minimum_rating}`);
+      queryString += `HAVING AVG(rating) >= $${queryParams.length}`;
+    }
+  
+    queryParams.push(limit);
+    queryString += `
+    ORDER BY cost_per_night
+    LIMIT $${queryParams.length};
+    `;
+
+    // 5
+    console.log(queryString, queryParams);
+  
+    // 6
+    return pool.query(queryString, queryParams).then((res) => res.rows);
 };
 
 /**
