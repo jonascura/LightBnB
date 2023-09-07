a7d382437c0a1d8c0aed93a46ae2e17b5259e05e
Jonas Cura <jonasm.cura@gmail.com.com>
2023-09-06 23:04:48 -0600

    Refactor getAllReservations

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -67,7 +67,26 @@ const addUser = function (user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const queryString = `SELECT reservations.*, properties.*, avg(property_reviews.rating) as average_rating
+  FROM reservations
+  JOIN properties ON properties.id = reservations.property_id
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id = $1
+  AND end_date < now()::date
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2;`;
+
+  const values = [guest_id, limit];
+
+  return pool.query(queryString, values)
+  .then((response) => {
+    return response.rows;
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+  
 };
 
 
